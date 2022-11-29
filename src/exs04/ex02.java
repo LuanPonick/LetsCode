5c6360da757aa6a40c717d3c384ed9107d57a981
Luan <107047713+LuanPonick@users.noreply.github.com>
2022-11-29 05:55:30 -0300

    Add files via upload

--- src/exs04/ex02.java
+++ src/exs04/ex02.java
@@ -5,36 +5,105 @@ import java.util.Scanner;
 public class ex02 {
     public static void main(String[] args) {
 
-        String tabuleiroDesenho[][] = new String[3][3];
+        String TabuleiroDesenho[][];
 
-        title();
-        tabuleiroDesenho = criarTabuleiro();
-        mostrarTabulerio(tabuleiroDesenho);
-        jogada();
+        Title();
+        TabuleiroDesenho = criarTabuleiro();
+        Boolean acabar = false;
+        do{
+            MostrarTabulerio(TabuleiroDesenho);
+            //fazer teste de ganhador
+            FazerJogadaPlayer1(TabuleiroDesenho);
+            System.out.printf("\n\n\n\n\n\n\n");
+            MostrarTabulerio(TabuleiroDesenho);
+            //teste de ganhador
+            FazerJogadaPlayer2(TabuleiroDesenho);
+        }while(!acabar);
 
 
     }
-
-    private static void jogada() {
+    private static String[][] FazerJogadaPlayer1(String Tabuleiro[][]) {
         Scanner lerJ = new Scanner(System.in);
-        String jogador1;
-        String jogador2;
 
-        double continuar = false;
+        boolean continuar = false;
+
 
         do{
-            System.out.println("\nDigite a cordenada letra e ao lado a cordenada numero EX( A1 )");
-            System.out.println("Jogador 1(x) digite a posição.");
-            jogador1 = lerJ.next();
-        }while();
+            String Jogada;
 
+                System.out.println("\nDigite a cordenada letra e ao lado a cordenada numero EX( A1 )");
+                System.out.println("Jogador 1(x) digite a posição.");
+
+                Jogada = lerJ.nextLine().toUpperCase().trim().replaceAll("\\s+","");
+                continuar = TestarJogada(Tabuleiro,Jogada);
+                if(!continuar){
+                    System.out.println("Valor impossivel!!!\nTente outro valor");
+                    MostrarTabulerio(Tabuleiro);
+                }else {
+                    Tabuleiro = SalvarJogada(Tabuleiro,Jogada,"X");
+
+                }
+          }while(!continuar);
+
+        return Tabuleiro;
+    }
+    private static String[][] FazerJogadaPlayer2(String Tabuleiro[][]) {
+        Scanner lerJ = new Scanner(System.in);
+
+        boolean continuar = false;
+
+
+        do{
+            String Jogada;
+
+            System.out.println("\nDigite a cordenada letra e ao lado a cordenada numero EX( A1 )");
+            System.out.println("Jogador 2(O) digite a posição.");
+
+            Jogada = lerJ.nextLine().toUpperCase().trim().replaceAll("\\s+","");
+            continuar = TestarJogada(Tabuleiro,Jogada);
+            if(!continuar){
+                System.out.println("Valor impossivel!!!\nTente outro valor");
+                MostrarTabulerio(Tabuleiro);
+            }else {
+                Tabuleiro = SalvarJogada(Tabuleiro,Jogada,"O");
+            }
+        }while(!continuar);
+
+        return Tabuleiro;
+    }
+    private static String[][] SalvarJogada(String[][] Tabulerio,String Jogada, String MarkerPlayer) {
+
+        int X = 0;
+        int Y = 0;
+
+        if(Jogada.charAt(0) == 'A'){
+            X = 1;
+        } else if (Jogada.charAt(0) == 'B') {
+            X = 2;
+        } else if (Jogada.charAt(0) == 'C') {
+            X = 3;
+        }
+
+        Y = Character.getNumericValue(Jogada.charAt(1));
+
+        Tabulerio[Y][X] = "| "+MarkerPlayer+" |";
+
+        return Tabulerio;
     }
 
-    private static void title() {
-        System.out.println("#################################"+
-                "\n O MELHOR JOGO DA VELHO DO ANO" +
-                    "\n#################################");
-
+    private static boolean TestarJogada(String[][] Tabuleiro, String Cordenada) {
+        return switch (Cordenada) {
+            case "A1" -> Tabuleiro[1][1].equals("| ✤ |");
+            case "A2" -> Tabuleiro[2][1].equals("| ✤ |");
+            case "A3" -> Tabuleiro[3][1].equals("| ✤ |");
+            case "B1" -> Tabuleiro[1][2].equals("| ✤ |");
+            case "B2" -> Tabuleiro[2][2].equals("| ✤ |");
+            case "B3" -> Tabuleiro[3][2].equals("| ✤ |");
+            case "C1" -> Tabuleiro[1][3].equals("| ✤ |");
+            case "C2" -> Tabuleiro[2][3].equals("| ✤ |");
+            case "C3" -> Tabuleiro[3][3].equals("| ✤ |");
+            default -> false;
+        };
     }
 
     private static String[][] criarTabuleiro() {
@@ -79,7 +148,7 @@ public class ex02 {
         return temp;
     }
 
-    private static void mostrarTabulerio(String [][] tabulerio) {
+    private static void MostrarTabulerio(String [][] tabulerio) {
         final String space ="   ";
         for(int i = 0; i < 16; i++){
 
@@ -103,4 +172,18 @@ public class ex02 {
         }
     }
 
+
+
+
+
+
+
+
+
+    private static void Title() {
+        System.out.println("#################################"+
+                "\n O MELHOR JOGO DA VELHO DO ANO" +
+                "\n#################################");
+
+    }
 }
