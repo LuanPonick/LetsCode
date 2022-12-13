3a7fdbe37993181f638276060016899300e8e451
LuanPonick <luaponick.sz.2@gmail.com>
2022-12-13 15:21:51 -0300

    updatte 2

--- src/exs05/ex04.java
+++ src/exs05/ex04.java
@@ -7,13 +7,24 @@ public class ex04 {
         double[] alturas = {1.90,1.70,1.40,1.20,1.60};
         double imcs[] = new double[5];
 
+        fazerCalculo(imcs,pesos,alturas);
+        mostrarPessoasSobrePeso(imcs,nomes);
+    }
+    public static double[] fazerCalculo(double[] imcs,int[] pesos,double[] alturas)
+    {
         for(int i = 0; i < imcs.length;i++){
             imcs[i] = ( pesos[i] / Math.pow(alturas[i],2));
         }
+        return imcs;
+    }
+    public static double[] mostrarPessoasSobrePeso(double imcs[], String nomes[]){
         for (int i = 0 ; i< imcs.length ; i++){
             if(imcs[i] < 18.5 || imcs[i]>24.9){
                 System.out.println(nomes[i] +" Esta fora do peso");
             }
         }
+        return imcs;
     }
+
+
 }
